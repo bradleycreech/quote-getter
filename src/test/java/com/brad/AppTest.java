b9483186751abdeee92ebe170ffdb264b4858436
Bradley <bcreech601@gmail.com>
2021-02-17 18:03:19 -0700

    Added Language mapper tests

--- src/test/java/com/brad/AppTest.java
+++ src/test/java/com/brad/AppTest.java
@@ -1,37 +1,32 @@
 package com.brad;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    static final String RANDOM_STRING = "sdfsdf";
+    static final String ENGLISH_LANGUAGE = "english";
+    static final String SERVICE_ENGLISH_LANGUAGE = "en";
+    static final String RUSSIAN_LANGUAGE = "russian";
+    static final String SERVICE_RUSSIAN_LANGUAGE = "ru";
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    public void testSanityCheck()
     {
         assertTrue( true );
+    }
+
+    public void testEnglishMapping(){
+        assertTrue(LanguageMapper.map(ENGLISH_LANGUAGE) == SERVICE_ENGLISH_LANGUAGE);
+    }
+
+    public void testRussianMapping(){
+        assertTrue(LanguageMapper.map(RUSSIAN_LANGUAGE) == SERVICE_RUSSIAN_LANGUAGE);
     }
 }
